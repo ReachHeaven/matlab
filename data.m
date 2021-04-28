9c3febff476871645edc4c44563cc531a43b9086
ReachHeaven <karamirov@gmail.com>
2021-04-28 19:25:08 +0500

    regression neurolink with delay

---
+++ data.m
@@ -0,0 +1,45 @@
+inData = inData(1:4000);
+outData = outData(1:4000);
+TrainDatainput = inData(1:2500);
+TrainTestinput = inData(2501:4000);
+TrainDataoutput = outData(1:2500);
+TrainTestoutput = outData(2501:4000);
+time = 1:4000;
+plot(time(1:2500), TrainDatainput, time(2501:end), TrainTestinput)
+plot(time(1:2500), TrainDataoutput, time(2501:end), TrainTestoutput)
+
+TDI = con2seq(TrainDatainput');
+TDO = con2seq(TrainDataoutput');
+TTI = con2seq(TrainTestinput');
+TTO = con2seq(TrainTestoutput');
+
+rng(5)
+
+net = timedelaynet(1:4,7);%при данных  параметрах, получена самая низкая погрешность
+net.divideFcn="divideblock";
+
+[Xs,Xi,Ai,Ts] = preparets(net,TDI,TDO);
+net = train(net,Xs,Ts,Xi,Ai);
+view(net)
+
+R = net(TDI);
+
+plot(cell2mat(TDO))
+hold on
+plot(cell2mat(R))
+
+
+Q = net(TTI);
+plot(cell2mat(TTO))%Для сравнения с тестовыми данными
+hold on
+plot(cell2mat(Q))
+
+
+
+[Y,Xf,Af] = net(Xs,Xi,Ai);
+perf = perform(net,Ts,Y);
+[netc,Xic,Aic] = closeloop(net,Xf,Af);
+view(netc)
+y2 = netc(in,Xic,Aic);
+view(net)
+net = train(net,Xs,Ts,Xi,Ai);
